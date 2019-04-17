0bc42b8d06d09c4f30491582651485d4e086f670
Mathieu de Gouville <mathieu@degouville.com>
2019-04-17 20:53:54 +0200

    Create Clock component and center the content

---
+++ components/Clock.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+.clock
+  p {{ time }}
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      time: ''
+    }
+  },
+
+  head() {
+    return {
+      title: this.time
+    }
+  },
+
+  methods: {
+    startClock() {
+      setInterval(() => this.updateTime(), 1000)
+    },
+    updateTime() {
+      this.time = new Date().toLocaleTimeString()
+    }
+  },
+
+  created() {
+    this.startClock()
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+@import url('https://fonts.googleapis.com/css?family=IBM+Plex+Mono')
+
+.clock
+  p
+    font-weight: bold
+    font-family: 'IBM Plex Mono', sans-serif
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,67 +1,27 @@
-<template>
-  <section class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        001
-      </h1>
-      <h2 class="subtitle">
-        1/100 100DaysChallenge
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
-          >Documentation</a
-        >
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-          >GitHub</a
-        >
-      </div>
-    </div>
-  </section>
+<template lang="pug">
+.container
+  clock.clock
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Clock from '~/components/Clock.vue'
 
 export default {
   components: {
-    Logo
+    Clock
   }
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+<style lang="sass">
+.container
+  margin: 0 auto
+  display: flex
+  height: 100vh
+  justify-content: center
+  align-items: center
+  text-align: center
+  font-size: 2em
+  @media(min-width: 1000px)
+    font-size: 10em
 </style>
